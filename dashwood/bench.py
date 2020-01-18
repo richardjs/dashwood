4d676d72288f10ef53138d38ba770b2a2c62dd72
Richard Schneider <richard@schneiderbox.net>
2020-01-17 23:14:10 -0600

    fix bug in children() benchmark

--- dashwood/bench.py
+++ dashwood/bench.py
@@ -4,12 +4,12 @@ from dashwood import state
 
 
 def benchmark():
-    code = 'state.children(s)'
-    n = 1000000
+    code = 'list(state.children(s))'
+    n = 1000
     t = timeit(code, '''\
 from dashwood import state
 s = state.initial()
-    ''', number=1000)
+    ''', number=n)
     print(f'{code}\t{t*1000*1000/n:.5f} µs')
 
     code = 'state.is_win(s)'
@@ -21,6 +21,15 @@ s = state.initial()
     print(f'{code}\t\t{t*1000*1000/n:.5f} µs')
 
     code = 'search.minimax(s, 2)'
+    n = 3
+    t = timeit(code, '''\
+from dashwood import state
+from dashwood import search
+s = state.initial()
+    ''', number=n)
+    print(f'{code}\t{t*1000/n:.2f} ms')
+
+    code = 'search.montecarlo(s, 1)'
     n = 5
     t = timeit(code, '''\
 from dashwood import state
